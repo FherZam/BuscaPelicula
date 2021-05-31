34b700f9fcdd2fcc4cb40a3378f22ffee36021fb
FherZam <jfernandozamora@gmail.com>
2021-05-31 12:45:28 -0600

    ok

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
         return {
             title: 'Peliculas Vue 3',
             movieData: {},
-            movieTitle: "Spider Man"
+            movieTitle: "Rocky",
+
+            movies: []
         }
     },
     mounted() {
@@ -14,10 +16,25 @@ const app = Vue.createApp({
             const search = this.movieTitle.toLowerCase().replace(/ /g, "+");
             console.log(search);
 
+            if (search===""){
+                alert('Debe especificar un nombre de película...')    
+                return;
+            }
+
             const data =await fetch(`https://www.omdbapi.com/?apikey=b09874c0&t=${search}`);
             const jsonData = await data.json();
             this.movieData = jsonData;
+
+            const dataLista = await fetch(`https://www.omdbapi.com/?apikey=b09874c0&s=${search}`);
+            const jsonDataLista = await dataLista.json();
+            console.log(jsonDataLista.Search[0])
+            this.movies = jsonDataLista.Search
             
+        },
+        BuscarTitulo(name){
+            this.movieTitle = name;
+            this.getMovie();
+
         }
     }
 
